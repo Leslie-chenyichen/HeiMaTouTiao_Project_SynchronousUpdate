72ba5eaf3698b35e4fc6c531b6b898da75df065f
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-03 18:30:53 +0800

    这个是脚手架下载完成,css引入,首页静态资源准备

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <div class="close">
+        <span class="iconfont iconicon-test"></span>
+      </div>
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="inputs">
+        <input placeholder="请输入手机号" class="input" />
+        <input placeholder="密码" class="input" type="password" />
+      </div>
+      <p class="tips">
+        没有账号？
+        <a href="#/register" class>去注册</a>
+      </p>
+      <div data-v-4bc01e24 class="button">登录按钮</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
