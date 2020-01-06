795d3e940dd162d2fdbc2a743d991c2a9e92bef3
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-06 22:24:00 +0800

    用户更改密码完成

--- src/views/EditPersonal.vue
+++ src/views/EditPersonal.vue
@@ -9,9 +9,13 @@
     </div>
     <hmcell title="昵称" :desc="currentUser.nickname" @click="nickshow=!nickshow"></hmcell>
     <van-dialog v-model="nickshow" title="修改昵称" show-cancel-button @confirm="updateNickname">
-      <van-field ref="nick"   :value="currentUser.nickname" placeholder="请输入昵称" required label="用户名" />
+      <van-field ref="nick" :value="currentUser.nickname" placeholder="请输入昵称" required label="用户名" />
     </van-dialog>
-    <hmcell title="密码" :desc="currentUser.password" type="password"></hmcell>
+    <hmcell title="密码" :desc="currentUser.password" type="password" @click="password=!password"></hmcell>
+    <van-dialog v-model="password" title="修改密码" show-cancel-button @confirm="updatePassword" :before-close="beforeClose">
+      <van-field ref="originalpass" placeholder="请输入原密码" required label="原密码" />
+      <van-field ref="newpassword" placeholder="请输入新密码" required label="新密码" />
+    </van-dialog>
     <hmcell title="性别" :desc="currentUser.gender===0?'女':'男'"></hmcell>
   </div>
 </template>
@@ -30,7 +34,8 @@ export default {
   data () {
     return {
       currentUser: {},
-      nickshow: false
+      nickshow: false,
+      password: false
     }
   },
   async mounted () {
@@ -73,6 +78,28 @@ export default {
     updateNickname () {
       // console.log(111111)
       console.log(this.$refs.nick.$refs.input.value)
+    },
+    updatePassword () {
+      let originalpass = this.$refs.originalpass.$refs.input.value
+
+      if (originalpass === this.currentUser.password) {
+        console.log(this.currentUser)
+      } else {
+        this.$toast.fail('输入原密码不正确')
+      }
+    },
+    beforeClose (action, done) {
+      // console.log(action)
+      if (action === 'confirm') {
+        let originalpass = this.$refs.originalpass.$refs.input.value
+        if (originalpass === !this.currentUser.password) {
+          this.$toast.fail('输入原密码不正确')
+          this.$refs.originalpass.$refs.input.focus()
+          done(false)
+        }
+      } else {
+        done()
+      }
     }
   }
 }
