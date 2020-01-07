5bfe2d2eacf8a6d19ed5243e0fed30bc389b420f
一生挚爱(Leslie) <chenyichen8532522@163.com>
2020-01-07 23:56:55 +0800

    用户性别修改成功

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js'
 // 引入全局样式
-import { Button, Toast, Icon, Uploader, Dialog, Field } from 'vant'
+import { Button, Toast, Icon, Uploader, Dialog, Field, Picker } from 'vant'
 Vue.use(Button)
   .use(Toast)
   .use(Icon)
   .use(Uploader)
   .use(Dialog)
   .use(Field)
+  .use(Picker)
 Vue.config.productionTip = false
 // import '@/styles/reset.css'
 new Vue({
--- src/views/EditPersonal.vue
+++ src/views/EditPersonal.vue
@@ -12,11 +12,20 @@
       <van-field ref="nick" :value="currentUser.nickname" placeholder="请输入昵称" required label="用户名" />
     </van-dialog>
     <hmcell title="密码" :desc="currentUser.password" type="password" @click="password=!password"></hmcell>
-    <van-dialog v-model="password" title="修改密码" show-cancel-button @confirm="updatePassword" :before-close="beforeClose">
+    <van-dialog
+      v-model="password"
+      title="修改密码"
+      show-cancel-button
+      @confirm="updatePassword"
+      :before-close="beforeClose"
+    >
       <van-field ref="originalpass" placeholder="请输入原密码" required label="原密码" />
       <van-field ref="newpassword" placeholder="请输入新密码" required label="新密码" />
     </van-dialog>
-    <hmcell title="性别" :desc="currentUser.gender===0?'女':'男'"></hmcell>
+    <hmcell title="性别" :desc="currentUser.gender===0?'女':'男'" @click="gendershow=!gendershow"></hmcell>
+      <van-dialog v-model="gendershow" title="修改性别" show-cancel-button @confirm="updateGender">
+      <van-picker :columns="['女','男']" :default-index="currentUser.gender" @change="onChange" />
+    </van-dialog>
   </div>
 </template>
 
@@ -35,7 +44,9 @@ export default {
     return {
       currentUser: {},
       nickshow: false,
-      password: false
+      password: false,
+      gendershow: false,
+      gender: ''
     }
   },
   async mounted () {
@@ -75,31 +86,56 @@ export default {
         this.$toast.fail('文件上传失败')
       }
     },
-    updateNickname () {
-      // console.log(111111)
-      console.log(this.$refs.nick.$refs.input.value)
+    // 用户名
+    async updateNickname () {
+      let name = this.$refs.nick.$refs.input.value
+      // 更新
+      let res = await updateUserById(this.currentUser.id, { nickname: name })
+      if (res.data.message === '修复成功') {
+        this.currenUser.nickname = name
+        this.$toast.success('修改成功')
+      } else {
+        this.$toast.fail('修改失败')
+      }
     },
+    // 更新密码
     updatePassword () {
       let originalpass = this.$refs.originalpass.$refs.input.value
 
       if (originalpass === this.currentUser.password) {
-        console.log(this.currentUser)
-      } else {
-        this.$toast.fail('输入原密码不正确')
+        let newpassword = this.$refs.newpassword.$refs.input.value
+        if (/^\S{3,6}$/.test(newpassword)) {
+
+        }
       }
     },
+    // 用户密码
     beforeClose (action, done) {
-      // console.log(action)
+      // 如果用户单击的的确认，那么就要判断原密码是否正确
       if (action === 'confirm') {
+        // 获取到用户输入的原密码
         let originalpass = this.$refs.originalpass.$refs.input.value
+        // 获取了原密码后就判断原密码对不对
         if (originalpass === !this.currentUser.password) {
-          this.$toast.fail('输入原密码不正确')
+          this.$toast.fail('输入原密码不正确') // 给用户弹出提示
+          this.$refs.originalpass.$refs.input.select()// 调出那个弹窗关闭
           this.$refs.originalpass.$refs.input.focus()
+          done(false)
+        } else if (/^\S{3,6}$/.test(this.$refs.newpassword.$refs.input.value)) {
+          this.$toast.fail('请你输入3-16位新的密码')
           done(false)
         }
       } else {
         done()
       }
+    },
+    // 修改性别
+    updateGender () {
+      console.log(this.gender)
+    },
+    onChange (picker, value, index) {
+      console.log(this.currentUser.gender)
+      this.$toast(`当前值：${value},当前索引：${index}`)
     }
   }
 }
